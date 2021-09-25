c961221473e393d6dee41bdfc56d85a4bdbcbd11
Febriyanto Nugroho <vmvm1071@gmail.com>
2021-09-25 08:07:04 +0700

    Create solve.asm

---
+++ random/solve.asm
@@ -0,0 +1,36 @@
+MOV A, 0x35
+MOV B, 0x35
+MOV D, 6
+
+loop:
+MOV [233], B
+MOV [232], A
+CALL satuan
+
+satuan:
+CMP B, 0x30
+MOV C, 1
+MOV C, 1
+MOV C, 1
+MOV C, 1
+MOV C, 1
+JC kurangi_B
+JE muter
+
+kurangi_B:
+SUB B, 1
+MOV C, 1
+MOV C, 1
+MOV C, 1
+JMP loop
+
+muter:
+SUB A, 1
+MOV B, 0x39
+DEC D
+CMP D, 0
+JNZ loop
+JZ henti
+
+henti:
+HLT
